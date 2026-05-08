ff2d3a6ed45b9af01472ab7c91cd772abd232953
Om Shukla <omshukla8573@gmail.com>
2026-05-08 18:10:08 +0530

    fixed railway deployment

--- app.py
+++ app.py
@@ -486,10 +486,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    import os
-
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=False
-    )
+    app.run(host="0.0.0.0", port=5000)
